542c2f9e30f56ed79a3637decda54b691d0d3e3c
Rahmouni Abdelhak <rahmouni.abdelhak.1@gmail.com>
2020-02-18 00:07:26 +0100

    impelement a dynamic solution with hashmap

--- solution.java
+++ solution.java
@@ -11,6 +11,9 @@ class solution {
     // output values
     private static String[] output;
 
+    // temporary values
+    private static HashMap<Long,String[]> foundResults = new HashMap<Long,String[]>();
+
     public static void main(String args[]){
         readInput();
         output = solveProblem(maxSlices, numberOfPizzaTypes-1);
@@ -18,34 +21,40 @@ class solution {
     }
 
     private static String[] solveProblem(int c, int i){
+        
+        Long key = cantorPairing(c,i);
 
-        String[] result = new String[2];
-
-        if(c==0 || i<0){
-            result[0] = String.valueOf(maxSlices - c);
-            result[1] = "";
-            return result;
-        }
-        else if(c < slicesOnEachPizza[i]){
-            result = solveProblem(c, i-1);
-            result[1] += "0";
-            return result;
-        }
+        if(foundResults.get(key)!=null) return foundResults.get(key);
         else {
-            String[] ordering = solveProblem(c-slicesOnEachPizza[i],i-1);
-            String[] notOrdering = solveProblem(c,i-1);
-            
-            if(Integer.valueOf(ordering[0])>Integer.valueOf(notOrdering[0])){
-                result[0] = ordering[0];
-                result[1] = ordering[1];
-                result[1] += "1";
+            String[] result = new String[2];
+            if(c==0 || i<0){
+                result[0] = String.valueOf(maxSlices - c);
+                result[1] = "";
+                return result;
+            }
+            else if(c < slicesOnEachPizza[i]){
+                result = solveProblem(c, i-1);
+                result[1] += "0";
+                foundResults.put(key,result);
+                return result;
             }
             else {
-                result[0] = notOrdering[0];
-                result[1] = notOrdering[1];
-                result[1] += "0"; 
+                String[] ordering = solveProblem(c-slicesOnEachPizza[i],i-1);
+                String[] notOrdering = solveProblem(c,i-1);
+                
+                if(Integer.valueOf(ordering[0])>Integer.valueOf(notOrdering[0])){
+                    result[0] = ordering[0];
+                    result[1] = ordering[1];
+                    result[1] += "1";
+                }
+                else {
+                    result[0] = notOrdering[0];
+                    result[1] = notOrdering[1];
+                    result[1] += "0";
+                }
+                foundResults.put(key,result);
+                return result;
             }
-            return result;
         }
     }
 
@@ -60,11 +69,24 @@ class solution {
     }
 
     private static void writeOutput(String output){
+        int numberOfPizzaOrdered = 0;
+        ArrayList<Integer> indexOfChosenPizza = new ArrayList<Integer>();
         for(int i=0;i<output.length();i++){
             if(output.charAt(i)=='1'){
-                System.out.print(i+" ");
+                numberOfPizzaOrdered++;
+                indexOfChosenPizza.add(i);
             }
+        }
+
+        System.out.println(numberOfPizzaOrdered);
+
+        for(int i=0;i<indexOfChosenPizza.size();i++){
+            System.out.print(indexOfChosenPizza.get(i)+" ");
         }
     }
 
+    private static long cantorPairing(float a, float b){
+        float r = ((a+b)/2)*(a+b+1)+b;
+        return (long) r;
+    }
 }
